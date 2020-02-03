cf4b2e963fc3436cc1ff78ded6e3be20e3c8a6eb
bvajhi2@ui <bvajhi2@uic.edu>
2020-02-03 11:31:48 -0600

    Added a helper to calculate pay for family A

--- src/com/babysitterinc/PayCalculator.java
+++ src/com/babysitterinc/PayCalculator.java
@@ -7,24 +7,38 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PayCalculator {
-    //Assumes the given to the class is 24 hour format
     public String convertTimeTo24Hour (String time){
         //Format of the date defined in the input String
         DateFormat df = new SimpleDateFormat("hh:mm aa");
         //Desired format: 24 hour format: Change the pattern as per the need
-        DateFormat outputformat = new SimpleDateFormat("HH:mm");
+        DateFormat outputFormat = new SimpleDateFormat("HH:mm");
         Date date = null;
         String output = null;
         try{
             //Converting the input String to Date
             date= df.parse(time);
             //Changing the format of date and storing it in String
-            output = outputformat.format(date);
+            output = outputFormat.format(date);
         }catch(ParseException pe){
             pe.printStackTrace();
         }
         return output;
     }
+
+   public int calculatePayForFamilyA(int startHour, int endHour){
+       int total = 0;
+       
+       if (endHour <= 23) {
+           total = (endHour - startHour) * 15;
+
+       } else if (startHour < 23 && endHour > 23) {
+           total = ((23 - startHour) * 15) + ((endHour - 23) * 20);
+       } else if (startHour > 23 && endHour > 23) {
+           total = (endHour - startHour) * 20;
+
+       }
+       return total;
+   }
 
 
 
--- test/PayCalculatorTests.java
+++ test/PayCalculatorTests.java
@@ -11,4 +11,13 @@ public class PayCalculatorTests {
         assertEquals("05:00", payCalculator.convertTimeTo24Hour("5:00 AM"));
     }
 
+    @Test
+    public void whenStartAndEndHoursArePassedIntoCalculatePayForFamilyAItShouldCalculateTheCorrectResult(){
+        PayCalculator payCalculator = new PayCalculator();
+        assertEquals(75, payCalculator.calculatePayForFamilyA(17,22));
+        assertEquals(150, payCalculator.calculatePayForFamilyA(17,26));
+        assertEquals(80, payCalculator.calculatePayForFamilyA(24,28));
+
+    }
+
 }
